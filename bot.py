faa7de8342a223389af6fdb17a743331ea14896c
hapsidra <hapsidra@gmail.com>
2020-05-05 20:56:13 +0900

    bot fix

--- bot.py
+++ bot.py
@@ -13,20 +13,34 @@ proxies = {
 
 def report():
   no_traffic_users = requests.get('http://84.201.149.40/no_traffic_users').json()
-  res = '<b>Абоненты с нулевым трафиком:</b>\n'
+  res = 'Абоненты с нулевым трафиком:\n'
   for user in no_traffic_users:
     res += user['login'] + ', '
   
   top_stop_users = requests.get('http://84.201.149.40/top_stop_users').json()
-  res += '\n\n<b>Абоненты с частой переавторизацией:</b>\n'
+  res += '\n\nАбоненты с частой переавторизацией:\n'
   for user in top_stop_users:
     res += user['login'] + ', '
 
   top_stop_switches = requests.get('http://84.201.149.40/top_stop_switches').json()
-  res += '\n\n<b>Маршрутизаторы с частой переавторизацией:</b>\n'
+  res += '\n\nМаршрутизаторы с частой переавторизацией:\n'
   for user in top_stop_switches:
     res += user['switch'] + ', '
+  
+  res += '\n\nСостояние устройств:\n'
   snmp = requests.get('http://84.201.149.40/snmp_stat').json()
+  for device_name in snmp:
+    device = snmp[device_name]
+    res += device_name + '\n'
+    if device['mean_temperature'] > 0:
+      res += 'Средняя температура:' + str(device['mean_temperature']) + '\n'
+    if device['current_temperature'] > 0:
+      res += 'Текущяя температура:' + str(device['current_temperature']) + '\n'
+    if device['mean_cpu'] > 0:
+      res += 'Средняя загрузка ЦПУ:' + str(device['mean_cpu']) + '\n'
+    if device['current_cpu'] > 0:
+      res += 'Текущяя загрузка ЦПУ:' + str(device['current_cpu']) + '\n'
+    res += '\n'
   requests.get('https://api.telegram.org/bot1109194190:AAEBfa3H6Rrl9S8qKTC2JA8VTVrz9j4-P74/sendMessage?chat_id=@vhack_report_channel&text=' + res, proxies=proxies)
 
 
